57fc9ddd59e13a2f8efb27e84954427bd7fe2065
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-02-28 00:01:49 +0100

    customer service

---
+++ services/customer/src/main/java/com/radeb/radebstore/CustomerApplication.java
@@ -0,0 +1,13 @@
+package com.radeb.radebstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerApplication.class, args);
+	}
+
+}
---
+++ services/customer/src/main/java/com/radeb/radebstore/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package com.radeb.radebstore.controller;
+
+import com.radeb.radebstore.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService service;
+}
