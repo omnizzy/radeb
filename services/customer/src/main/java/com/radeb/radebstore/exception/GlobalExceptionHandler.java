81abe9ac0812ed2267c30753ecc6851485182899
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-03-02 22:33:55 +0100

    exception

--- services/customer/src/main/java/com/radeb/radebstore/exception/GlobalExceptionHandler.java
+++ services/customer/src/main/java/com/radeb/radebstore/exception/GlobalExceptionHandler.java
@@ -3,9 +3,12 @@ package com.radeb.radebstore.exception;
 import com.radeb.radebstore.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler  {
@@ -19,8 +22,16 @@ public class GlobalExceptionHandler  {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handle(MethodArgumentNotValidException  exp) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError)error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(exp.getMsg());
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(errors));
     }
 }
