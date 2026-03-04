ba4c5818c47f08a7b19a210adf876b9e2573cf68
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-03-04 23:36:56 +0100

    product config

---
+++ services/product/src/main/java/com/radeb/radebstore/category/Category.java
@@ -0,0 +1,24 @@
+package com.radeb.radebstore.category;
+
+import com.radeb.radebstore.model.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
