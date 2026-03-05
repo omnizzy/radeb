5439bb3f178217ac065cfc7e3c56bf8697163618
omnizzy <118431825+omnizzy@users.noreply.github.com>
2026-03-05 12:39:33 +0100

    prd

---
+++ services/product/src/main/java/com/radeb/radebstore/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.radeb.radebstore.controller;
+
+import com.radeb.radebstore.dto.ProductRequest;
+import com.radeb.radebstore.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(
+            @RequestBody @Valid ProductRequest request
+    ){
+
+    }
+}
